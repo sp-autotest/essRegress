ee47ca8dc93a673e5cf11260622885eb8824154f
mycolam <mycolam@yandex.ru>
2018-09-06 17:03:20 +0300

    Включен раздел 8

--- src/test/java/EssTest.java
+++ src/test/java/EssTest.java
@@ -583,7 +583,7 @@ public class EssTest {
     @Description("Карта VISA;\nНаправление перелета: туда-обратно;\n" +
     "Состав бронирования авиаперелета, билеты: 1 взрослый, 2 ребенка;" +
     "Дополнительные услуги: «Выбор мест», «Питание», «Мед.страховка», «Авто», «Аэроэкспресс», «Трансфер»")
-    @Test(priority = 8, description = "Раздел 8", groups = {"part8"}, dataProvider= "data8", enabled = false)
+    @Test(priority = 8, description = "Раздел 8", groups = {"part8"}, dataProvider= "data8")
     public void section8(String browser, String resolution, String language, String currency, String city) {
         runBrowser(browser, resolution);
         int test = 8;
